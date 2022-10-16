56a1236cfbfffd11a961ce7f647209fff0fe8349
Kemunto Gift <giftoyugi@gmail.com>
2022-10-16 21:48:04 +0300

    added functions to create tasks and trigger modal form for update

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ let formValidation = () => {
     if (textInput.value === "") {
         console.log("failure");
         msg.innerHTML = "Task cannot be blank";
+    } else if (textarea.value === "") {
+        console.log("failure");
+        msg.innerHTML = "Please add a description";
     } else {
         console.log("success");
         msg.innerHTML = "";
@@ -26,4 +29,41 @@ let formValidation = () => {
             add.setAttribute("data-bs-dismiss", "");
         })();
     }
+};
+
+
+let data = [{}];
+
+let acceptData = () => {
+    data.push({
+        text: textInput.value,
+        date: dateInput.value,
+        description: textarea.value,
+    });
+
+    localStorage.setItem("data", JSON.stringify(data));
+
+    console.log(data);
+    createTasks();
+};
+
+
+let createTasks = () => {
+    tasks.innerHTML = "";
+    data.map((x, y) => {
+        return (tasks.innerHTML += `
+    <div id=${y}>
+          <span class="fw-bold">${x.text}</span>
+          <span class="small text-secondary">${x.date}</span>
+          <p>${x.description}</p>
+  
+          <span class="options">
+            <i onClick= "editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fas fa-edit"></i>
+            <i onClick ="deleteTask(this);createTasks()" class="fas fa-trash-alt"></i>
+          </span>
+        </div>
+    `);
+    });
+
+    resetForm();
 };
