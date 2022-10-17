d5110284835a1a8bb41f4cc2a76f35173793e720
Kemunto Gift <giftoyugi@gmail.com>
2022-10-17 09:45:24 +0300

    fixed modal buttons bug

--- app.js
+++ app.js
@@ -58,6 +58,7 @@ let createTasks = () => {
           <p>${x.description}</p>
   
           <span class="options">
+          <button class="completed-btn"><i class="fas fa-check"></i></button>
             <i onClick= "editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fas fa-edit"></i>
             <i onClick ="deleteTask(this);createTasks()" class="fas fa-trash-alt"></i>
           </span>
